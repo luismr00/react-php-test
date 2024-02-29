0a7da746b616a11f9a731c13ca0bdaf5f2102590
Luis Rangel <luismrangel7@gmail.com>
2024-02-29 12:37:40 -0600

    added basic server request

--- PHP/server.php
+++ PHP/server.php
@@ -3,49 +3,12 @@
     ini_set('display_errors', 1);
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: *');
-    // header('Access-Control-Allow-Methods: *')
-    // $user = $_POST['name'];
-    // echo ("Hello from server: $user");
-
-//     $json = file_get_contents('php://input');
-//     $data = json_decode($json);
-//     echo ($data);
-
-    // echo ("HI");
-    // print "Hello world!";
-
-    // echo '<script>console.log("Welcome to GeeksforGeeks!"); </script>'; 
-    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //     $request_raw = file_get_contents('php://input');
-    //     $request = json_decode($request_raw);
-    //     foreach ($request as $key => $value) {
-    //             echo "{$key}: {$value}\n";
-    //     }
-    //     exit;
-    // }
-
-    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //     error_log(print_r($_REQUEST, true));
-    //     error_log(print_r($_SERVER, true));
-    //     error_log(print_r($_POST['title'], true));
         
-    //     $var = $_POST['title'];
-    //     error_log(print_r($var, true));
-    //     return;
-        
-    // }
-        
-    $dataToReturn = [];
+    // $dataToReturn = [];
 
-    // populate the data as you need
+    //Retrives the data from the client side with json_deconde...
     $dataToReturn = json_decode(file_get_contents('php://input'));
-    // echo ("Your data: $dataToReturn");
-    // foreach ($request as $key => $value) {
-    //         echo "{$key}: {$value}\n";
-    // }
-    // echo ("Testing msg");
-    // Output the JSON string with appropriate header
     header('Content-Type: application/json; charset=utf-8');
-    echo json_encode(["success" => $dataToReturn]);
+    echo json_encode(["success" => $dataToReturn]); //json_encode sends back data to the client
     exit(0);
 // ?>
